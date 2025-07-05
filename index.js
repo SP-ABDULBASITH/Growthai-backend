e2c7568b156cc9423beff49e0e4f0874fb43472e
SP-ABDUL-BASITH <spabdulbasith2002@gmail.com>
2025-07-05 19:58:54 +0530

    fix: update API to live backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const generateBusinessData = require('./utils/generateBusinessData');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
